e8a0445b2ec0ebc97d216a3421c493044a987e55
Federico Papaantuono <federicopapaantuono@Federicos-MacBook-Air.local>
2026-04-20 13:27:41 +0200

    edit

--- Lab_02/Scripts/script_tcp.sh
+++ Lab_02/Scripts/script_tcp.sh
@@ -1,27 +1,77 @@
 #!/bin/bash
 
+NETWORK_SCENARIO="eth-eth"
+REVERSE_MODE="no"
+
 SERVER_IP="192.168.1.49"
-IFACE="en5" 
+SERVER_OS="Ubuntu Linux 22.04"
+SERVER_IFACE="enpls0f0"
+SERVER_LINK_SPEED="1000Mb/s"
+SERVER_MTU="1500"
+
+CLIENT_IFACE=""
+CLIENT_LINK_SPEED="" 
+
+CLIENT_OS=$(sw_vers -productName 2>/dev/null)" "$(sw_vers -productVersion 2>/dev/null)
+IPERF_VERSION=$(iperf3 -v | awk 'NR==1')
+CLIENT_IP=$(ipconfig getifaddr "$CLIENT_IFACE")
+CLIENT_BASE_MTU=$(ifconfig "$CLIENT_IFACE" | grep -o "mtu [0-9]*" | awk '{print $2}')
 
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 OUT_DIR="../Results"
 mkdir -p "$OUT_DIR"
 
 OUTPUT_FILE="${OUT_DIR}/tcp_gput_${TIMESTAMP}.dat"
-TMP_FILE="tmp_tcp_gput.dat"
+INFO_FILE="${OUT_DIR}/tcp_gput_${TIMESTAMP}_info.txt"
+TMP_FILE="tmp_gput.dat"
 
 MIN_MTU=500
 MAX_MTU=1500
 STEP_MTU=200
 
-ITERATIONS=5
-TEST_DURATION=1
+ITERATIONS=10
+TEST_DURATION=10
 SLEEP_TIME=1
+
+cat <<EOF > "$INFO_FILE"
+========================================
+TEST CONFIGURATION METADATA
+========================================
+Timestamp: $TIMESTAMP
+Scenario: $NETWORK_SCENARIO
+Reverse Mode (-R): $REVERSE_MODE
+
+[ TEST PARAMETERS ]
+Test Type: TCP (MTU Sweep)
+MTU Min: $MIN_MTU Bytes
+MTU Max: $MAX_MTU Bytes
+MTU Step: $STEP_MTU Bytes
+Iterations per Step: $ITERATIONS
+Time per Iteration (-t): $TEST_DURATION seconds
+
+[ CLIENT INFO ]
+Interface: $CLIENT_IFACE
+IP Address: $CLIENT_IP
+OS: $CLIENT_OS
+Base MTU: $CLIENT_BASE_MTU
+Link Speed: $CLIENT_LINK_SPEED
+
+[ SERVER INFO ]
+IP Address: $SERVER_IP
+Interface: $SERVER_IFACE
+OS: $SERVER_OS
+Base MTU: $SERVER_MTU
+Link Speed: $SERVER_LINK_SPEED
+
+[ SOFTWARE ]
+Tool: $IPERF_VERSION
+========================================
+EOF
 
 echo "# MTU(Bytes) Avg(Mbps) Min(Mbps) Max(Mbps) StdDev" > "$OUTPUT_FILE"
 
 for mtu in $(seq $MIN_MTU $STEP_MTU $MAX_MTU); do
-    sudo ifconfig "$IFACE" mtu "$mtu" > /dev/null 2>&1
+    sudo ifconfig "$CLIENT_IFACE" mtu "$mtu" > /dev/null 2>&1
     sleep 3 
     
     rm -f "$TMP_FILE"
@@ -29,7 +79,6 @@ for mtu in $(seq $MIN_MTU $STEP_MTU $MAX_MTU); do
     for i in $(seq 1 $ITERATIONS); do
         iperf3 -c "$SERVER_IP" -t "$TEST_DURATION" | \
             grep "receiver" | tr -s ' ' | cut -d ' ' -f 7 >> "$TMP_FILE"
-        
         sleep "$SLEEP_TIME"
     done
     
@@ -51,7 +100,7 @@ for mtu in $(seq $MIN_MTU $STEP_MTU $MAX_MTU); do
 done
 
 rm -f "$TMP_FILE"
-sudo ifconfig "$IFACE" mtu 1500 > /dev/null 2>&1
+sudo ifconfig "$CLIENT_IFACE" mtu 1500 > /dev/null 2>&1
 
 echo "You can print results with:"
 echo "gnuplot -p -e 'plot \"$OUTPUT_FILE\" using 1:(\$2-\$5):3:4:(\$2+\$5) with candlesticks'"
